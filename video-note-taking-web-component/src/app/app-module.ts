023e310ff3e10b1acb4d4dae780731499c79fe68
NagyBendeguz <bendeguz@medea.hu>
2026-02-06 21:24:39 +0100

    use manual bootstrapping

--- video-note-taking-web-component/src/app/app-module.ts
+++ video-note-taking-web-component/src/app/app-module.ts
@@ -1,12 +1,17 @@
-import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationRef, ComponentFactoryResolver, CUSTOM_ELEMENTS_SCHEMA, Injector, NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { VideoPlayer } from './video-player/video-player';
+import { VideoNavbar } from './video-navbar/video-navbar';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    VideoPlayer,
+    VideoNavbar
   ],
   imports: [
     BrowserModule,
@@ -15,6 +20,24 @@ import { App } from './app';
   providers: [
     provideBrowserGlobalErrorListeners(),
   ],
-  bootstrap: [App]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  //bootstrap: [App]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(private injector: Injector, private appRef: ApplicationRef, private componentFactoryResolver: ComponentFactoryResolver) {
+    // Átalakítani egy egyedi web komponensre.
+    const videoPlayerElement = createCustomElement(VideoPlayer, { injector });
+    const videoNavbarElement = createCustomElement(VideoNavbar, { injector });
+
+    // Lehessen használni egyedi web komponensként.
+    customElements.define('video-player-element', videoPlayerElement);
+    customElements.define('video-navbar-element', videoNavbarElement);
+  }
+  
+  // Manuális Bootstrap.
+  ngDoBootstrap() {
+    const appComponent = this.componentFactoryResolver.resolveComponentFactory(App);
+    this.appRef.bootstrap(appComponent);
+  }
+}
