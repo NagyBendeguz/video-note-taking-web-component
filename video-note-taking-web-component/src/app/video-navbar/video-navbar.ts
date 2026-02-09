60396931fcb6ae73eb56ec663b5a460b1e72f6ec
NagyBendeguz <bendeguz@medea.hu>
2026-02-09 13:24:17 +0100

    add mute and unmute function for volume button

--- video-note-taking-web-component/src/app/video-navbar/video-navbar.ts
+++ video-note-taking-web-component/src/app/video-navbar/video-navbar.ts
@@ -10,6 +10,7 @@ import { Video } from '../services/video';
 export class VideoNavbar {
   isPlaying: boolean = false;
   volumePercentage: number = 100;
+  tempVolumePercentage: number = 100;
   isFullscreen: boolean = false;
   @Output() togglePlay = new EventEmitter<void>();
   @Output() rewind = new EventEmitter<void>();
@@ -41,6 +42,17 @@ export class VideoNavbar {
 
   setVolume(value: string): void {
     this.volumePercentage = Number(value);
+    this.videoService.setVolume(this.volumePercentage);
+  }
+
+  mute(): void {
+    this.tempVolumePercentage = this.volumePercentage;
+    this.volumePercentage = 0;
+    this.videoService.setVolume(this.volumePercentage);
+  }
+
+  unMute(): void {
+    this.volumePercentage = this.tempVolumePercentage;
     this.videoService.setVolume(this.volumePercentage);
   }
 
