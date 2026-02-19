ecdae0df5a3c1b3a795713f06ab723ac220bffb2
NagyBendeguz <bendeguz@medea.hu>
2026-02-19 17:19:12 +0100

    add shortcuts class

---
+++ video-note-taking-web-component/src/app/models/shortcuts.spec.ts
@@ -0,0 +1,7 @@
+import { Shortcuts } from './shortcuts';
+
+describe('Shortcuts', () => {
+  it('should create an instance', () => {
+    expect(new Shortcuts()).toBeTruthy();
+  });
+});
---
+++ video-note-taking-web-component/src/app/models/shortcuts.ts
@@ -0,0 +1,18 @@
+export class Shortcuts {
+    note: string = "";
+    settings: string = "";
+    focusOnInput: string = "";
+    thumbnailMoveForward: string = "";
+    thumbnailMoveBackward: string = "";
+    save: string = "";
+    cancel: string = "";
+    edit: string = "";
+    delete: string = "";
+    bold: string = "";
+    italic: string = "";
+    underline: string = "";
+    strikethrough: string = "";
+    orderedList: string = "";
+    unOrderedList: string = "";
+    table: string = "";
+}
