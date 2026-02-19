d7986d75119966a93adfa5310a76657b01034e11
NagyBendeguz <bendeguz@medea.hu>
2026-02-19 17:18:51 +0100

    add settings class

---
+++ video-note-taking-web-component/src/app/models/settings.ts
@@ -0,0 +1,13 @@
+export class Settings {
+    language: string = "";
+    thumbnailResolution: string = "";
+    convertInput: boolean = false;
+    theme: string = "";
+    confirmCancel: boolean = false;
+    confirmDelete: boolean = false;
+    defaultFolderLocation: string = "";
+    videoMoveRate: number = 0;
+    thumbnailMoveRate: number = 0;
+    autoSave: boolean = false;
+    fontSize: number = 0;
+}
