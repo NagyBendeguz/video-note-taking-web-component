f6a697991911d7c388fd577d8990e66b3be160f4
NagyBendeguz <bendeguz@medea.hu>
2026-02-21 22:33:52 +0100

    refact code

--- video-note-taking-web-component/src/app/services/entry.ts
+++ video-note-taking-web-component/src/app/services/entry.ts
@@ -9,11 +9,22 @@ export class EntryService {
   private entrySource = new BehaviorSubject<Entry>(new Entry());
   entry$ = this.entrySource.asObservable();
 
+  private arrayEntrySource = new BehaviorSubject<Array<Entry>>(new Array<Entry>());
+  arrayEntry$ = this.arrayEntrySource.asObservable();
+
   getEntry(): Observable<Entry> {
     return this.entry$;
   }
 
   setEntry(entry: Entry): void {
     this.entrySource.next(entry);
+  }
+
+  getArrayEntry(): Observable<Array<Entry>> {
+    return this.arrayEntry$;
+  }
+
+  setArrayEntry(arrayEntry: Array<Entry>): void {
+    this.arrayEntrySource.next(arrayEntry);
   }
 }
--- video-note-taking-web-component/src/app/video-note/video-note.ts
+++ video-note-taking-web-component/src/app/video-note/video-note.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Entry } from '../models/entry';
+import { EntryService } from '../services/entry';
 
 @Component({
   selector: 'app-video-note',
@@ -7,5 +10,11 @@ import { Component } from '@angular/core';
   styleUrl: './video-note.sass',
 })
 export class VideoNote {
+  arrayEntry$: Observable<Array<Entry>> = new Observable<Array<Entry>>();
 
+  constructor(private entryService: EntryService) {}
+
+  ngAfterViewInit(): void {
+    this.arrayEntry$ = this.entryService.getArrayEntry();
+  }
 }
